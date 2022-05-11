e13665ee2cdda4f73b75e7e3740b4b1c8ca681dd
Vladimir Pankratov <sollex.uz@gmail.com>
2022-05-11 17:53:23 +0500

    Test code

--- components/effects/__init__.py
+++ components/effects/__init__.py
@@ -1,16 +1,28 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components.light.types import AddressableLightEffect
+from esphome.components.light.effects import register_addressable_effect
+from esphome.const import CONF_NAME, CONF_UPDATE_INTERVAL
 
-AUTO_LOAD = true
+effects_ns = cg.esphome_ns.namespace("effects")
+AddressableMatrixEffect = effects_ns.class_(
+    "AddressableMatrixEffect", AddressableLightEffect
+)
 
-CONF_MY_REQUIRED_KEY = 'my_required_key'
-CONF_MY_OPTIONAL_KEY = 'my_optional_key'
+CONFIG_SCHEMA = cv.All(cv.Schema({}), cv.only_with_arduino)
 
-CONFIG_SCHEMA = cv.Schema({
-  cv.Required(CONF_MY_REQUIRED_KEY): cv.string,
-  cv.Optional(CONF_MY_OPTIONAL_KEY, default=10): cv.int_,
-}).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+@register_addressable_effect(
+    "addressable_matrix",
+    AddressableMatrixEffect,
+    "Matrix",
+    {
+        cv.Optional(
+            CONF_UPDATE_INTERVAL, default="255ms"
+        ): cv.positive_time_period_milliseconds,
+    },
+)
+async def addressable_matrix_effect_to_code(config, effect_id):
+    var = cg.new_Pvariable(effect_id, config[CONF_NAME])
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    return var

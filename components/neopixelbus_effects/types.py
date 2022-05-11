e221bd5bae01dd6e262ca870c089b2aa2e89cd57
Vladimir Pankratov <sollex.uz@gmail.com>
2022-05-11 17:24:11 +0500

    Test line

--- components/neopixelbus_effects/types.py
+++ components/neopixelbus_effects/types.py
@@ -6,6 +6,6 @@ light_ns = cg.esphome_ns.namespace("light")
 # Effects
 LightEffect = light_ns.class_("LightEffect")
 AddressableLightEffect = light_ns.class_("AddressableLightEffect", LightEffect)
-AddressableLambdaLightEffect = light_ns.class_(
+AddressableMatrixEffect = light_ns.class_(
     "AddressableMatrixEffect", AddressableLightEffect
 )

aa20d3a284ab4b3797a0fa97d221eb5afaaaed1a
Vladimir Pankratov <sollex.uz@gmail.com>
2022-05-12 14:30:32 +0500

    Simplified code

--- components/effects/AddressableRainEffect.h
+++ components/effects/AddressableRainEffect.h
@@ -21,25 +21,27 @@ public:
         this->last_run_ = now;
 
         for (uint8_t x = 0; x < this->manager_->width(); x++) {
+            const int led_num = getPixelNumber(x, this->manager_->height() - 1);
+
             // заполняем случайно верхнюю строку
-            const Color thisColor = it[getPixelNumber(x, this->manager_->height() - 1)].get();
+            const Color thisColor = it[led_num].get();
             if (!this->colorcode(thisColor)) {
                 if (random(0, 50) == 0) {
                     if (this->scale_ <= 1) {
-                        it[getPixelNumber(x, this->manager_->height() - 1)] = light::ESPHSVColor(random(0, 9) * 28, 255, 255);
+                        it[led_num] = light::ESPHSVColor(random(0, 9) * 28, 255, 255);
                     } 
                     else if (this->scale_ == 100) {
-                        it[getPixelNumber(x, this->manager_->height() - 1)] = RgbToHsv(Color(0xE0FFFF - 0x101010 * random(0, 4)));
+                        it[led_num] = RgbToHsv(Color(0xE0FFFF - 0x101010 * random(0, 4)));
                     } 
                     else {
-                        it[getPixelNumber(x, this->manager_->height() - 1)] = light::ESPHSVColor(this->scale_ * 2.4 + random(0, 16), 255, 255);
+                        it[led_num] = light::ESPHSVColor(this->scale_ * 2.4 + random(0, 16), 255, 255);
                     }
                 }
             } 
             else {
                 // Shade pixel
-                Color color = it[getPixelNumber(x, this->manager_->height() - 1)].get();
-                it[getPixelNumber(x, this->manager_->height() - 1)] = color - light::ESPHSVColor(0, 0, random(96, 128)).to_rgb();
+                Color color = it[led_num].get();
+                it[led_num] = RgbToHsv(color - light::ESPHSVColor(0, 0, random(96, 128)).to_rgb());
             }
         }
         // сдвигаем всё вниз

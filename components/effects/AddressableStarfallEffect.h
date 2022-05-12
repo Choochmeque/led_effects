98dbd09dc8bc7b616e7ba66eb3136513bd774085
Vladimir Pankratov <sollex.uz@gmail.com>
2022-05-12 16:16:45 +0500

    Test code

--- components/effects/AddressableStarfallEffect.h
+++ components/effects/AddressableStarfallEffect.h
@@ -69,8 +69,7 @@ protected:
     void fadePixel(light::AddressableLight &it, uint8_t x, uint8_t y, uint8_t step)
     {
         const uint16_t pixelNum = getPixelNumber(x, y);
-        light::ESPColorView cv = it[pixelNum];
-        Color color(cv.get_red_raw(), cv.get_green_raw(), cv.get_blue_raw());
+        Color color = it[pixelNum].get();
 
         if (isEqualColors(color, Color::BLACK)) {
             return;

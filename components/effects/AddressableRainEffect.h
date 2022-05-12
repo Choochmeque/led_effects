d5037b416483b3b107a7d01405a213dd66bf4520
Vladimir Pankratov <sollex.uz@gmail.com>
2022-05-12 14:14:17 +0500

    Added rain effect

---
+++ components/effects/AddressableRainEffect.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "AddressableAbstractEffect.h"
+
+
+namespace esphome {
+namespace effects {
+
+class AddressableRainEffect : public AddressableAbstractEffect 
+{
+public:
+    AddressableRainEffect(const std::string &name) : AddressableAbstractEffect(name) {}
+
+    void apply(light::AddressableLight &it, const Color &current_color) override 
+    {
+        const uint32_t now = millis();
+        if (now - this->last_run_ < this->update_interval_) {
+            return;
+        }
+
+        this->last_run_ = now;
+
+        for (uint8_t x = 0; x < this->manager_->width(); x++) {
+            // заполняем случайно верхнюю строку
+            const Color thisColor = it[getPixelNumber(x, this->manager_->height() - 1)].get();
+            if (!this->colorcode(thisColor)) {
+                if (random8(0, 50) == 0) {
+                    if (this->scale_ <= 1) {
+                        it[getPixelNumber(x, this->manager_->height() - 1)] = light::ESPHSVColor(random(0, 9) * 28, 255, 255);
+                    } 
+                    else if (this->scale_ == 100) {
+                        it[getPixelNumber(x, this->manager_->height() - 1)] = CRGB(0xE0FFFF - 0x101010 * random(0, 4));
+                    } 
+                    else {
+                        it[getPixelNumber(x, this->manager_->height() - 1)] = light::ESPHSVColor(this->scale_ * 2.4 + random(0, 16), 255, 255);
+                    }
+                }
+            } 
+            else {
+                // Shade pixel
+                it[getPixelNumber(x, this->manager_->height() - 1)] -= light::ESPHSVColor(0, 0, random(96, 128));
+            }
+        }
+        // сдвигаем всё вниз
+        for (uint8_t x = 0; x < this->manager_->width(); x++) {
+            for (uint8_t y = 0; y < this->manager_->height() - 1; y++) {
+                it[getPixelNumber(x, y)] = it[getPixelNumber(x, y + 1)];
+            }
+        }
+
+        it.schedule_show();
+    }
+};
+
+}  // namespace effects
+}  // namespace esphome
--- components/effects/__init__.py
+++ components/effects/__init__.py
@@ -21,6 +21,9 @@ AddressableLightersEffect = effects_ns.class_(
 AddressableMatrixEffect = effects_ns.class_(
     "AddressableMatrixEffect", AddressableLightEffect
 )
+AddressableRainEffect = effects_ns.class_(
+    "AddressableRainEffect", AddressableLightEffect
+)
 AddressableRainbowEffect = effects_ns.class_(
     "AddressableRainbowEffect", AddressableLightEffect
 )
@@ -121,6 +124,29 @@ async def addressable_matrix_effect_to_code(config, effect_id):
 
     var = cg.new_Pvariable(effect_id, config[CONF_NAME])
     cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    cg.add(var.set_manager(mngr))
+    return var
+
+@register_addressable_effect(
+    "addressable_rain",
+    AddressableRainEffect,
+    "Rain",
+    {
+        cv.GenerateID(CONF_EMNGR_ID): cv.use_id(EffectsManagerComponent),
+        cv.Optional(
+            CONF_UPDATE_INTERVAL, default="100ms"
+        ): cv.positive_time_period_milliseconds,
+        cv.Optional(
+            "scale", default="20"
+        ): cv.int_range(0, 255),        
+    },
+)
+async def addressable_rain_effect_to_code(config, effect_id):
+    mngr = await cg.get_variable(config[CONF_EMNGR_ID])
+
+    var = cg.new_Pvariable(effect_id, config[CONF_NAME])
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    cg.add(var.set_scale(config["scale"]))
     cg.add(var.set_manager(mngr))
     return var
 

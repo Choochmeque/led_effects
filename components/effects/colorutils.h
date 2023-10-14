ffcf564ddd705c1d6d21266a3dc563339cbafce1
Vladimir Pankratov <sollex.uz@gmail.com>
2023-10-14 17:48:57 +0500

    Fixed build

--- components/effects/colorutils.h
+++ components/effects/colorutils.h
@@ -120,13 +120,13 @@ public:
     CRGBPalette16( const TProgmemRGBPalette16& rhs)
     {
         for( uint8_t i = 0; i < 16; ++i) {
-            entries[i] =  pgm_read_dword_near( rhs + i);
+            entries[i] = esphome::Color(pgm_read_dword_near(rhs + i));
         }
     }
     CRGBPalette16& operator=( const TProgmemRGBPalette16& rhs)
     {
         for( uint8_t i = 0; i < 16; ++i) {
-            entries[i] =  pgm_read_dword_near( rhs + i);
+            entries[i] = esphome::Color(pgm_read_dword_near(rhs + i));
         }
         return *this;
     }
